96d2562331f4a8f0b4f0476980616367fbbbe699
André Varandas <andre.m.varandas@gmail.com>
2019-08-13 23:39:17 +0100

    fix(PostCard) Display only posts that published

--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -1,11 +1,9 @@
 <template>
   <Layout>
-    <h1 class="tag-title text-center space-bottom">
-      # {{ $page.tag.title }}
-    </h1>
+    <h1 class="tag-title text-center space-bottom"># {{ $page.tag.title }}</h1>
 
     <div class="posts">
-      <PostCard v-for="edge in $page.tag.belongsTo.edges" :key="edge.node.id" :post="edge.node"/>
+      <PostCard v-for="edge in $page.tag.belongsTo.edges" :key="edge.node.id" :post="edge.node" />
     </div>
   </Layout>
 </template>
@@ -19,6 +17,7 @@ query Tag ($id: String!) {
         node {
           ...on Post {
             title
+            published
             path
             date (format: "D. MMMM YYYY")
             timeToRead
@@ -34,8 +33,8 @@ query Tag ($id: String!) {
 </page-query>
 
 <script>
-import Author from '~/components/Author.vue'
-import PostCard from '~/components/PostCard.vue'
+import Author from "~/components/Author.vue";
+import PostCard from "~/components/PostCard.vue";
 
 export default {
   components: {
@@ -43,12 +42,11 @@ export default {
     PostCard
   },
   metaInfo: {
-    title: 'Hello, world!'
+    title: "Hello, world!"
   }
-}
+};
 </script>
 
 <style lang="scss">
-
 </style>
 
