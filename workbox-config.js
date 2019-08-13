d66db1354f7d297c06316933ebbc47f2ebef2d7b
André Varandas <andre.m.varandas@gmail.com>
2019-08-14 00:04:27 +0100

    adds workbox to generate service worker automagically

---
+++ workbox-config.js
@@ -0,0 +1,9 @@
+/**
+ * Shamelessly stolen from
+ * https://github.com/gridsome/gridsome/issues/167
+ */
+module.exports = {
+  globDirectory: "dist/",
+  globPatterns: ["**/*.{html,css,js,png,json}"],
+  swDest: "dist/sw.js"
+};
